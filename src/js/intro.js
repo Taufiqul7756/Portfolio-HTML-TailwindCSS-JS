fc2597dd14b2994e2e3cbfc51b43dd3846fc837f
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-03-25 01:58:21 +0600

    Introduction - Dynamic

---
+++ src/js/intro.js
@@ -0,0 +1,18 @@
+fetch("./src/data/intro.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const introSection = document.getElementById("introSection");
+    data.forEach((intro) => {
+      const introduction = `
+      <h2 class="text-yellow-400 font-bold text-xs">INTRODUCTION</h2>
+      <p class="font-bold lg:text-3xl md:text-3xl sm:text-2xl text-white">${intro.firstLineTitle}</p>
+      <p class="font-bold lg:text-3xl md:text-3xl sm:text-2xl text-white">${intro.secondLineTitle}</p>
+      <p class="mt-2">${intro.age} years /  ${intro.name} / ${intro.location}</p>
+      <p class="mt-5 text-gray-300">${intro.des}</p>
+      
+      <button class="rounded-full font-bold bg-yellow-400 hover:bg-yellow-600 px-4 py-1.5 mt-10 text-md">Download CV <i class="fa-solid fa-download"></i></button>
+    `;
+      introSection.innerHTML = introduction;
+    });
+  })
+  .catch((error) => console.error("Error fetching introduction data:", error));
